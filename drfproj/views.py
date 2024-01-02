e8793cac5b16be147ad4305b99ec4f59b155edb8
shuvo-art <shuvo83qn@gmail.com>
2024-01-03 01:50:26 +0600

    Authentication a user before using API in Django rest framework

--- drfproj/views.py
+++ drfproj/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from drfapp.serializers import StudentSerializer
 from drfapp.models import Student
 
 class TesView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+    
     def get(self, request, *args, **kwargs):
         qs = Student.objects.all()
         student1 = qs.first()
